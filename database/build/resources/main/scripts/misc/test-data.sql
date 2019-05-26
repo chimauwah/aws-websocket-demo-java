561df8bb12041eef7491f2a942f33a4fc15d25af
Chima Uwah <cuwah@shutterfly.com>
2019-05-25 20:50:58 -0400

    remove ignored files

--- database/build/resources/main/scripts/misc/test-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO ws_demo.connections (connection_id) VALUES ('1');
-INSERT INTO ws_demo.connections (connection_id) VALUES ('2');
-INSERT INTO ws_demo.connections (connection_id) VALUES ('3');
